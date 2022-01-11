929292e3c7c4f845a66a9f9d3d385fe247ef20a3
lucapiccinelli <luca.picci@gmail.com>
2022-01-11 17:04:48 +0100

    complex variable parsing

--- pycture/picture.py
+++ pycture/picture.py
@@ -1,3 +1,6 @@
+import re
+import functools as ft
+
 class Picture:
     def __init__(self, name, length):
         self.name = name
@@ -20,5 +23,20 @@ def read_picture(picture_string):
     return Picture(picture_string_tokens[1], picture_len(picture_string_tokens[3]))
 
 def picture_len(picture_definition):
-    return len(picture_definition)
-    
+    matches = re.findall(r'v?([\dxz]+)(\(\s*(\d+)\s*\))*', picture_definition)
+    if matches is None:
+        return 0
+
+    return ft.reduce(lambda acc, match: acc + calculate_len(match), matches, 0)
+
+def calculate_len(match):
+    repeted_chars = match[0]
+    num_in_parenthesis = match[2]
+
+    length = len(repeted_chars)
+    if num_in_parenthesis != '':
+        length += int(num_in_parenthesis) - 1
+
+    return length
+
+
--- tests/test_read_cobol_picture_tests.py
+++ tests/test_read_cobol_picture_tests.py
@@ -6,7 +6,7 @@ import pytest
     ('77 banana pic 99', pyc.Picture('banana', 2)),
     ('77     banana   pic    99', pyc.Picture('banana', 2)),
     (' 77 banana   pic    99   ', pyc.Picture('banana', 2)),
-    # (' 77 banana   pic    9(02)   ', pyc.Picture('banana', 2)),
+    (' 77 banana   pic    9(02)   ', pyc.Picture('banana', 2)),
 ])
 def test_can_convert_a_cobol_picture_to_a_python_dictonary(picture, expected_result):
     actual_result = pyc.read_picture(picture)
@@ -15,7 +15,20 @@ def test_can_convert_a_cobol_picture_to_a_python_dictonary(picture, expected_res
 @pytest.mark.parametrize('picture_definition, expected_result', [
     ['9', 1],
     ['99', 2],
-    # ['9(02)', 2],
+    ['9(02)', 2],
+    ['9( 03 )', 3],
+    ['9( 03)', 3],
+    ['9(00003)', 3],
+    ['9(3)', 3],
+    ['99(3)', 4],
+    ['xxxx', 4],
+    ['xxx9', 4],
+    ['x(2)', 2],
+    ['z(2)', 2],
+    ['xx(2)', 3],
+    ['9(2)v9', 3],
+    ['9(2)v99(02)', 5],
+    ['9(2)v9(040)', 42],
 ])
 def test_can_parse_the_length_of_a_picture(picture_definition, expected_result):
     actual_result = pyc.picture_len(picture_definition)
