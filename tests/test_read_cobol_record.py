66d7593c3eab37972df72de9ed921ca0526a371f
lucapiccinelli <luca.picci@gmail.com>
2022-01-12 18:12:54 +0100

    failing tree test

--- tests/test_read_cobol_record.py
+++ tests/test_read_cobol_record.py
@@ -23,6 +23,20 @@ from pycture import picture as pyc
             pyc.Picture('pera', 2, 2),
             pyc.Picture('mela', 3, 2))
     ),
+    (
+        """01 banana.
+                02 pera.
+                    03 seed pic 9(2)
+                    03 fruit pic x(20)
+                02 mela pic xxx.
+        """,
+        pyr.Record('banana', 1,
+            pyr.Record('pera', 2,
+                pyc.Picture('seed', 2, level = 3),
+                pyc.Picture('fruit', 20, level = 3)
+            ),
+            pyc.Picture('mela', 3, level = 2))
+    ),
 ])
 def test_can_convert_a_cobol_picture_to_a_python_dictonary(record, expected_result):
     actual_result = pyr.read_record(record)
