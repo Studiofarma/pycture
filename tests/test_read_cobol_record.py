ab806f7fe4e15ae2748b749ada28ddc92ebfa259
lucapiccinelli <luca.picci@gmail.com>
2022-01-13 15:49:03 +0100

    test reading complex structure

--- tests/test_read_cobol_record.py
+++ tests/test_read_cobol_record.py
@@ -37,6 +37,32 @@ from pycture import picture as pyc
             ),
             pyc.Picture('mela', 3, level = 2))
     ),
+    ('77 banana pic 9', pyc.Picture('banana', 1)),
+    (' 01 pera   pic    9(03)v9(2)   ', pyc.Picture('pera', 5, 1)),    
+    (
+        """01 banana.
+                02 pera.
+                    03 fruit.
+                        05 juice pic xx.
+                        05 taste pic z(6).
+                    03 seed pic 9(2).
+                02 mela.
+                    03 seed pic 9(2).
+                    03 fruit pic x(20)..
+        """,
+        pyr.Record('banana', 1,
+            pyr.Record('pera', 2,
+                pyr.Record('fruit', 3,
+                    pyc.Picture('juice', 2, level = 5),
+                    pyc.Picture('taste', 6, level = 5)
+                ),
+                pyc.Picture('seed', 2, level = 3),
+            ),
+            pyr.Record('mela', 2,
+                pyc.Picture('seed', 2, level = 3),
+                pyc.Picture('fruit', 20, level = 3)
+            ))
+    ),
 ])
 def test_can_convert_a_cobol_picture_to_a_python_dictonary(record, expected_result):
     actual_result = pyr.read_record(record)
