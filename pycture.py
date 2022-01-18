092dd630c4b25f1d6b42bb0e086194b11018e4f6
lucapiccinelli <luca.picci@gmail.com>
2022-01-18 12:21:23 +0100

    prints error message

--- pycture.py
+++ pycture.py
@@ -10,13 +10,16 @@ def main(args):
     definition_filename = args.definition_filename
     output_filename = args.output if args.output is not None else "out.csv"
 
-    data_lines = count_file_lines(data_filename)
     definition_file_text = read_file(definition_filename)
 
     with open(data_filename, 'r', encoding='utf-8') as datafile_iterator:
         record = pyr.read_record(definition_file_text)
         record_structure = record.structure
 
+        if args.verbose:
+            print(record_structure)
+
+        data_lines = count_file_lines(data_filename)
         with tqdm(total=data_lines) as progress_bar:
             def update_bar(i, _, line_out):
                 progress_bar.set_description(f'Processed line {i}')
@@ -54,6 +57,13 @@ if __name__ == "__main__":
     parser.add_argument(
         '-o', '--output',  nargs='?',
         help='the filename to give to the output file')
+    parser.add_argument(
+        '-v', '--verbose',  action='store_true',
+        help='display more parsing informations, like the interpreted Cobol picture')
     args = parser.parse_args()
 
-    main(args)
+    try:
+        main(args)
+    except Exception as e:
+        print(e)
+        parser.print_help()
