4661bdcf56c165184f9015f190f158309f3ef33c
lucapiccinelli <luca.picci@gmail.com>
2022-01-18 12:11:44 +0100

    updated main file

--- pycture.py
+++ pycture.py
@@ -3,28 +3,32 @@
 import argparse
 from tqdm import tqdm
 from pycture import conversion
+from pycture import record as pyr
 
 def main(args):
     data_filename = args.data_filename
     definition_filename = args.definition_filename
     output_filename = args.output if args.output is not None else "out.csv"
 
-    definition_file_text = read_file(definition_filename)
+    data_lines = count_file_lines(data_filename)
     definition_file_text = read_file(definition_filename)
 
-    data_lines = count_file_lines(data_filename)
-    with tqdm(total=data_lines) as progress_bar:
-        def update_bar(i, _, line_out):
-            progress_bar.set_description(f'Processed line {i}')
-            progress_bar.update()
-            return line_out
-        
-        csv_text = conversion.converto_file_to_csv(
-            definition_filename,
-            data_filename,
-            row_listner_fn = update_bar)
-        
-        write_to_output(output_filename, csv_text)
+    with open(data_filename, 'r', encoding='utf-8') as datafile_iterator:
+        record = pyr.read_record(definition_file_text)
+        record_structure = record.structure
+
+        with tqdm(total=data_lines) as progress_bar:
+            def update_bar(i, _, line_out):
+                progress_bar.set_description(f'Processed line {i}')
+                progress_bar.update()
+                return line_out
+
+            csv_text_iterator = conversion.convert_iterator_to_csv(
+                record_structure, 
+                datafile_iterator,
+                row_listner_fn = update_bar)
+
+            write_to_output(output_filename, csv_text_iterator)
 
 def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:
