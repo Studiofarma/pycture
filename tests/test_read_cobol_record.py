5f575f2f916dfd76df7f22ebe31280259d7b5b41
lucapiccinelli <luca.picci@gmail.com>
2022-01-19 15:59:33 +0100

    new redefines examples

--- tests/test_read_cobol_record.py
+++ tests/test_read_cobol_record.py
@@ -211,6 +211,37 @@ def test_can_read_a_cobol_record(record, expected_result):
                 pyc.Picture('pera', 2, 2),
                 pyr.Record('xx', 2, pyc.Picture('xx-1', 1, 3), pyc.Picture('xx-2', 1, 3))))
     ),
+    (
+        """01 banana.
+                02 pera.
+                    03 pera-1 pic 9.
+                    03 pera-2 pic x.
+                02 xx redefines pera.
+                    03 xx-1 pic x.
+                    03 xx-2 pic 9.
+        """,
+        pyr.Record('banana', 1,
+            pyr.Redefines(
+                pyr.Record('pera', 2, pyc.Picture('pera-1', 1, 3), pyc.Picture('pera-2', 1, 3)),
+                pyr.Record('xx', 2, pyc.Picture('xx-1', 1, 3), pyc.Picture('xx-2', 1, 3))))
+    ),
+    (
+        """01 banana.
+                02 pera.
+                    03 pera-1 pic 9.
+                    03 pera-2 pic x.
+                02 xx redefines pera.
+                    03 xx-1 pic x.
+                    03 xx-2 pic 9.
+                02 yy pic 99.
+        """,
+        pyr.Record('banana', 1,
+            pyr.Redefines(
+                pyr.Record('pera', 2, pyc.Picture('pera-1', 1, 3), pyc.Picture('pera-2', 1, 3)),
+                pyr.Record('xx', 2, pyc.Picture('xx-1', 1, 3), pyc.Picture('xx-2', 1, 3))),
+            pyc.Picture('yy', 2, 2)
+        )
+    ),
 ])
 def test_can_read_a_cobol_record_with_redefines(record, expected_result):
     actual_result = pyr.read_record(record)
